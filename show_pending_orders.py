1de1aa009133e6adbf4e9c70369f9a8443d35958
Matt Strum <mstrum@gmail.com>
2018-05-01 17:26:23 -0700

    Add quote information to pending orders

--- show_pending_orders.py
+++ show_pending_orders.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 
-import argparse
 from collections import defaultdict
 from datetime import datetime
 from decimal import Decimal
 from math import ceil
-
-from dateutil.parser import parse
-import pytz
 
 from robinhood.exceptions import NotFound
 from robinhood.RobinhoodCachedClient import RobinhoodCachedClient, FORCE_LIVE
@@ -29,16 +25,21 @@ def display_pending_orders():
   instruments = client.get_instruments(instrument_ids)
   instrument_by_id = { i['id']: i for i in instruments }
 
+  quotes = client.get_quotes(instrument_ids, cache_mode=FORCE_LIVE)
+  quote_by_id = { get_last_id_from_url(quote['instrument']): quote for quote in quotes }
+
   pending_orders_by_instrument = defaultdict(list)
   for order in pending_orders:
     instrument_id = get_last_id_from_url(order['instrument'])
     pending_orders_by_instrument[instrument_id].append(order)
 
+  order_amounts = Decimal(0)
   for instrument_id, instrument_orders in pending_orders_by_instrument.items():
     instrument = instrument_by_id[instrument_id]
 
     print('{} ({})'.format(instrument['symbol'], instrument['simple_name'] or instrument['name']))
 
+    # Show position
     try:
       position = client.get_position_by_instrument_id(instrument['id'])
     except NotFound:
@@ -53,6 +54,17 @@ def display_pending_orders():
         print('\tcurrent position\t\t{} @ ${:.2f}'.format(
           position_quantity, position_average_buy_price, position_equity_cost))
 
+    # Show quote
+    quote = quote_by_id[instrument_id]
+    last_trade_price = Decimal(quote['last_trade_price'])
+    bid_price = Decimal(quote['bid_price'])
+    bid_size = int(quote['bid_size'])
+    ask_price = Decimal(quote['ask_price'])
+    ask_size = int(quote['ask_size'])
+    print('\tspread:\t${:.2f} ({}) <-> ${:.2f} ({})'.format(bid_price, bid_size, ask_price, ask_size))
+    print('\tlast:\t${:.2f}'.format(last_trade_price))
+
+    # Show orders
     for order in instrument_orders:
       order_id = order['id']
       order_state = order['state']
@@ -60,9 +72,18 @@ def display_pending_orders():
       order_side = order['side']
       order_quantity = int(float(order['quantity']))
       order_price = Decimal(order['price'])
+      multiplier = 1
+      order_total = order_quantity * order_price
+      if order_side == 'sell':
+        order_amounts -= order_total
+      else:
+        order_amounts += order_total
 
       print('\t{}\t{} {}\t{} @ ${:.2f}\t({})'.format(
         order_state, order_type, order_side, order_quantity, order_price, order_id))
+
+  print()
+  print('Combined order totals (negative means added account value) = ${:.2f}'.format(order_amounts))
 
 if __name__ == '__main__':
   display_pending_orders()
